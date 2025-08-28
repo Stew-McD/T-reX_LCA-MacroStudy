1232dd0b85d77c62b1abde0b1f1b31d94d471d94
SC-McD <stewart.charles.mcdowall@protonmail.ch>
2025-08-28 16:39:33 +0200

    update

--- code/06_data_processing.py
+++ code/06_data_processing.py
@@ -4,5 +4,53 @@ import pandas as pd
 
 FILE_RAW = "/home/stew/code/gh/T-reX_LCA-MacroStudy/data/05_Calculations_output//markets_1000_combined_rawresults_df.pickle"
 
+FILE_ACTIVITIES = "/home/stew/code/gh/T-reX_LCA-MacroStudy/data/04_Filter_output/activities_list_merged_T-reX_macro_markets.csv"
 
+FILE_COOKED = "/home/stew/code/gh/T-reX_LCA-MacroStudy/data/06_Data_processing_output/markets_1000_combined_cookedresults_df.csv"
+
+
+## Load data from calculations
 df_raw = pd.read_pickle(FILE_RAW)
+
+df_raw_columns_drop = ['parameters full', 'emptied', 'log parameters', 'activity type']
+
+df_raw = df_raw.drop(columns=df_raw_columns_drop)
+
+df_raw["Total waste (kg)"] = df_raw["Total (kg)"] + 1000*df_raw["Total (m3)"]
+df_raw = df_raw.drop(columns=["Total (kg)", "Total (m3)"])
+
+df_raw["Hazardous waste (kg)"] = df_raw["Hazardous (kg)"] + 1000*df_raw["Hazardous (m3)"]
+df_raw = df_raw.drop(columns=["Hazardous (kg)", "Hazardous (m3)", "location", "reference product", "unit"])
+
+
+## Load data from activity filtering
+
+df_acts = pd.read_csv(FILE_ACTIVITIES, sep=";")
+df_acts = df_acts.drop(columns=["production amount", "activity type", "ISIC_num", "ISIC_name", "CPC_num", "CPC_name"])
+
+## Merge the df_acts and df_raw based on the columns "code" and "database"
+
+df_merged = pd.merge(df_acts, df_raw, left_on=["code", "database", "name"], right_on=["code", "database", "name"], how="inner")
+
+## Process the dataframe for visualisation
+
+# Limit to activities with a unit of kg or m3
+
+df_merged = df_merged[df_merged["unit"].isin(["kilogram", "cubic meter"])]
+
+# replace kilogram with kg and cubic meter with m3
+df_merged["unit"] = df_merged["unit"].replace({"kilogram": "kg", "cubic meter": "m3"})
+
+
+# set ecoinvent baseline year to 2020
+df_merged["database"] = df_merged["database"].replace({"ecoinvent-3.9.1-cutoff":"ecoinvent-default-2020"})
+
+# add column for year based on the column "database"
+df_merged["Year"] = df_merged["database"].str.split("-").str[2]
+df_merged["Database"] = df_merged["database"].str.split("-").str[0:2].str.join("-")
+
+
+# output the final dataframe
+
+df_cooked = df_merged.copy()
+df_cooked.to_csv(FILE_COOKED, sep=";", index=True)
